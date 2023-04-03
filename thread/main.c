145d4e68270e471d4bb0436e54c3cb122793e839
ydl <yang623601391@gmail.com>
2023-04-03 09:35:01 +0800

    update

--- thread/main.c
+++ thread/main.c
@@ -1,31 +1,44 @@
 #include <pthread.h>
 #include <stdio.h>  
+#include <stdint.h>
+
+#define THREAD_NUMBERS 10
 
 int sum = 0;
 pthread_mutex_t mut;
 
-void *add(void *args) {
+void *add(void *_num) {
+    int num = (intptr_t)_num;
+    printf("thread num: %d, %p, %ld\n", num, &num, pthread_self());
     for (int i = 0; i < 10000; i++) {
         // pthread_mutex_lock(&mut);
         // sum++;
         // pthread_mutex_unlock(&mut);
         __sync_add_and_fetch(&sum, 1);
     }
+    return NULL;
 }
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    pthread_t t1;
-    pthread_t t2;
-    pthread_t t3;
-    pthread_t t4;
-
-    pthread_create(&t1, NULL, add, NULL);
-    pthread_create(&t2, NULL, add, NULL);
-    pthread_create(&t3, NULL, add, NULL);
-    pthread_create(&t4, NULL, add, NULL);
-    getchar();
+    // pthread_t t1;
+    // pthread_t t2;
+    // pthread_t t3;
+    // pthread_t t4;
+    void * thread_result;
+    pthread_t threads[THREAD_NUMBERS];
+    for (int i = 0; i < THREAD_NUMBERS; i++) {
+        int j = i;
+        // printf("thead: %d\n", j);
+        pthread_create(&threads[i], NULL, add, (void *)(intptr_t)j);
+    }
+    
+    for (int i = 0; i < THREAD_NUMBERS; i++) {
+        // printf("join thead: %d\n", i);
+        pthread_join(threads[i], NULL);
+    }
     printf("sum = %d\n", sum);
+
     return 0;
 }
